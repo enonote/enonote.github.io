781d1edc5d5561a47e1cc4bc1a80f0e1e4dd52f9
enonote <enokixut@gmail.com>
2017-12-17 02:35:44 +0900

    BookInfoClass分離

---
+++ js/userscript/mangamura_BookInfoClass.js
@@ -0,0 +1,160 @@
+// Require jQuery2.2.4
+
+// BookInfoクラス
+class BookInfo {
+  constructor(){
+    this._bookId = "";
+    this._bookTitle = "";
+    this._fileUrlList = [];
+  }
+  set bookId(bookId){
+    this._bookId = bookId;
+    if( this._bookId == null ){
+      console.log("(BookInfo Class)Invalid value: bookIdにnullがセットされました。");
+    }
+  }
+  get bookId(){
+    return this._bookId;
+  }
+  set bookTitle(bookTitle){
+    this._bookTitle = bookTitle;
+    if( bookTitle != null ){
+      this._bookTitle = bookTitle.trim();
+    }else{
+      console.log("(BookInfo Class)Invalid parameter: bookTitleにnullが渡されました。");
+    }
+  }
+  get bookTitle(){
+    return this._bookTitle;
+  }
+  get fileUrlList(){
+    return this._fileUrlList;
+  }
+  
+  // BookIdからファイルのURLを取得
+  resolveFileUrl(){
+    let dfd = $.Deferred();
+    let that = this;
+    
+    _getRequestKey(that.bookId).done(function(reqKey){
+      _getFileList(that.bookId ,reqKey).done(function(fileList){
+        for( let key in fileList ){
+          let data = fileList[key];
+          let fileUrl = data.img + "?" + "h="+data.h + "&t="+data.t;
+          that.fileUrlList.push(fileUrl);
+        }
+        
+        dfd.resolve();
+      }).fail(function(){
+        dfd.resolve();
+      });
+    }).fail(function(){
+      dfd.resolve();
+    });
+    
+    // 10秒でタイムアウト
+    setTimeout(function(){
+      if( dfd.state() == "pending" ){
+        alert("ERROR: resolveFileUrl リクエストがタイムアウト");
+        dfd.resolve();
+      }
+    },1000*60);
+    
+    return dfd.promise();
+
+    // VIEWERページのHTMLを取得し、KEYを抽出
+    function _getRequestKey(bookId){
+      console.log("DEBUG: _getRequestKey bookId = "+bookId);
+      var dfd = $.Deferred();
+      
+      var datas = $.ajax({
+            type: 'GET',
+            datatype: 'html',
+            url: "http://mangamura.org/kai_pc_viewer?p=" + bookId
+        }).done(function(data){
+            console.log("DEBUG: _getRequestKey done");
+            parseViewerPage(data,bookId).done(function(obj){
+                dfd.resolve(obj.key);
+            }).fail(function(){
+                console.log("ERROR：リクエストキーの取得に失敗");
+                dfd.reject();
+            });
+        }).fail(function(){
+            console.log("ERROR：ビュワーの取得に失敗");
+            dfd.reject();
+        }).always(function(){
+        });
+
+        return dfd.promise();
+
+        // 取得したHTMLからKEYを抽出
+        function parseViewerPage(htmlStr,bookId){
+          var dfd = $.Deferred();
+          
+          console.log("DEBUG: parseViewerPage");
+          console.log(htmlStr);
+
+          console.log("DEBUG: parseViewerPage send getRequestKey request.");
+          $.ajax({
+            url: "http://mangamura.org/pages/getxb",
+            type: 'get',
+            dataType: 'text',
+            contentType: 'application/json; charset=utf-8',
+            scriptCharset: 'utf-8',
+            data:{
+              wp_id: bookId
+            }
+          }).done(function(data){
+            console.log("DEBUG: parseViewerPage done");
+
+            // データチェック
+            var r = data.match(/([A-z0-9]{13,13})/);
+            console.log(r);
+            if( r ){
+              dfd.resolve({"id": bookId, "key": data});
+            }else{
+              console.log("ERROR: parseViewerPage データチェックエラー");
+              dfd.reject();
+            }
+            
+          }).fail(function(){
+            console.log("DEBUG: parseViewerPage リクエストキーの取得に失敗");
+            dfd.reject();
+          });
+
+          // 10秒でタイムアウト
+          setTimeout(function(){
+            if( dfd.state() == "pending" ){
+              alert("ERROR: parseViewerPage リクエストがタイムアウト");
+              dfd.resolve();
+            }
+          },1000*60);
+          
+          return dfd.promise();
+        }
+    }
+    
+    // ファイル一覧取得リクエスト送信
+    function _getFileList(bookId,key){
+        var dfd = $.Deferred();
+
+        var datas = $.ajax({
+            type: 'GET',
+            datatype: 'json',
+            url: "http://mangamura.org/pages/xge5?id="+bookId+"&x="+key
+        }).done(function(data){
+            dfd.resolve(data);
+
+        }).fail(function(){
+            console.log("ERROR：リクエストキーの取得に失敗");
+            dfd.reject();
+
+        }).always(function(){
+            dfd.resolve();
+        });
+
+        return dfd.promise();
+    }
+
+  }
+}
